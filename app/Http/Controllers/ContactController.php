b9cf3932761dcb25c232e83b557abae85da08647
Busra <busra.basci@zarcony.com>
2019-08-11 18:01:21 +0300

    test

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Contact;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('contacts.index',compact('contacts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view('contacts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+            $validatedData = $request->validate([
+                'first_name'=>'required|max:255',
+                'last_name'=>'required|alpha_num',
+                'email'=>'required',
+                'city'=>'required',
+                'country'=>'required',
+                'job_title'=>'required',
+                ]);
+                $contact=Contact::create($validatedData);
+
+                return redirect('/contact')->with('success','contact saved');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $contact = Contact::findOrFail('$id');
+        return view('contact.edit',compact('contact'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validatedData=$request->validate([
+            'first_name'=>'required|max:255',
+            'last_name'=>'required|alpha_num',
+            'email'=>'required',
+            'city'=>'required',
+            'country'=>'required',
+            'job_title'=>'required'
+            ]);
+            Contact::whereId($id)->update($validatedData);
+            return redirect('contact')->with('success','congratulation');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+        return view('contact')->with('success','Contact deleted.');
+    }
+}
